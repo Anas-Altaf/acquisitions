9d37dfa619700c77da8afd70bc82d513e1618491
Anas-Altaf <anasaltaf35@gmail.com>
2026-01-20 11:13:00 +0500

    initial commit with folder structure

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// About Express Js App
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello from Acquisitions...");
+});
+export default app;
